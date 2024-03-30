e07c0828d1624ba9233f809440ba5075b8296050
Fabricio Santos <fgsantoz@outlook.com>
2024-03-30 01:37:42 -0300

    lidando com erros

---
+++ adivinhacao.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    // imprime o cabeçalho do nosso jogo
+    printf("********************************************\n");
+    printf("* Bem vindo ao nosso Jogo de Adivinhação *\n");
+    printf("********************************************\n");
+
+}
